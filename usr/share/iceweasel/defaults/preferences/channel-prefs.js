1b6fd584df01343cda37d14bb7bdd288209dc86b
Gibbio <gibbio@gmail.com>
2013-05-15 12:53:37 +0200

    RPi-TC ARMv6j release

---
+++ usr/share/iceweasel/defaults/preferences/channel-prefs.js
@@ -0,0 +1,2 @@
+//@line 2 "/opt/build/iceweasel-10.0.12esr/browser/app/profile/channel-prefs.js"
+pref("app.update.channel", "default");
---
+++ usr/share/iceweasel/defaults/preferences/firefox-l10n.js
@@ -0,0 +1,4 @@
+//@line 36 "/opt/build/iceweasel-10.0.12esr/browser/locales/en-US/firefox-l10n.js"
+
+//@line 38 "/opt/build/iceweasel-10.0.12esr/browser/locales/en-US/firefox-l10n.js"
+
