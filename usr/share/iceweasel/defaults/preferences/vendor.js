351e1d87abb6fa586acb650f5ea2fb9fa430a2be
Gibbio <gibbio@gmail.com>
2013-11-20 11:54:23 +0100

    RPi-TC ARMv6j release 5

--- usr/share/iceweasel/defaults/preferences/vendor.js
+++
@@ -1,6 +0,0 @@
-//@line 2 "/opt/build/iceweasel-17.0.8esr/debian/vendor.js.in"
-pref("general.useragent.compatMode.firefox", true);
-//@line 4 "/opt/build/iceweasel-17.0.8esr/debian/vendor.js.in"
-pref("distribution.searchplugins.defaultLocale", "en-US");
-// Forbid application updates
-lockPref("app.update.enabled", false);
